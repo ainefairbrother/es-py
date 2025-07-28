7ab996cd3a88df95b4b74bcb03b86efc45affe1c
Aine Fairbrother-Browne <42773574+ainefairbrother@users.noreply.github.com>
2025-07-28 16:29:51 +0100

    feat: add utils for super_population

--- index/super_population_index/fetch_information_from_db.py
+++ index/super_population_index/fetch_information_from_db.py
@@ -60,19 +60,3 @@ class FetchSPFromDB:
         )
 
         return superpopulation
-
-
-def create_the_dictionary_structure() -> dict[str, Any]:
-    """Create the dictionary structure
-
-    Returns:
-        dict[str, Any]: New dictionary
-    """
-    superpopulation = {
-        "elasticId": None,
-        "name": None,
-        "display_colour": None,
-        "display_order": None,
-    }
-
-    return superpopulation
---
+++ index/super_population_index/utils.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+def create_the_dictionary_structure() -> dict[str, Any]:
+    """Create the dictionary structure
+
+    Returns:
+        dict[str, Any]: New dictionary
+    """
+    superpopulation = {
+        "elasticId": None,
+        "name": None,
+        "display_colour": None,
+        "display_order": None,
+    }
+
+    return superpopulation
