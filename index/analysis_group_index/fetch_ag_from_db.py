f1fb7ca2bd6d266993981855aa49db499b95c826
Aine Fairbrother-Browne <42773574+ainefairbrother@users.noreply.github.com>
2025-07-28 16:30:14 +0100

    feat: add utils for analysis_group

--- index/analysis_group_index/fetch_ag_from_db.py
+++ index/analysis_group_index/fetch_ag_from_db.py
@@ -61,20 +61,3 @@ class FetchAGFromDB:
         )
 
         return analysis_group
-
-
-def create_the_dictionary_structure() -> dict[str, Any]:
-    """Creates the dictionary structure for the analysis group
-
-    Returns:
-        dict[str, Any]: An initialized dictionary
-    """
-
-    analysis_group = {
-        "code": None,
-        "description": None,
-        "shortTitle": None,
-        "displayOrder": None,
-    }
-
-    return analysis_group
